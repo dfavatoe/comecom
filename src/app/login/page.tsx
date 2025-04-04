fbf078680edfb11607d96d57d6aeef1075645850
dfavatoe <d.favato@outlook.com>
2025-04-04 16:07:53 +0200

    Created new paragraph Diogo

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,7 +1,12 @@
 import React from "react";
 
 function page() {
-  return <div>Diogo</div>;
+  return (
+    <>
+      <div>Diogo</div>
+      <p>New paragraph from Diogo</p>
+    </>
+  );
 }
 
 export default page;
