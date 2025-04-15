4e7382841049729099b0c48e11a06043003f54b6
dfavatoe <d.favato@outlook.com>
2025-04-15 15:33:00 +0200

    Account2 branch to Test Branch (#14)
    * From Test to Main (#13)
    
    * Product branch to Test Branch (#11)
    
    * Created the api product  route, product page and changed the ProductT id to '_id'.
    
    * Added routes api/ products/[id], api/shopping-list/
            src/app/lib/actions.ts, src/app/lib/actions.ts
            src/app/products/[productId]/
            src/components/Grid.tsx
            src/components/ModalAlert.tsx
            src/components/ProductCard.tsx
            src/components/addToShoppingListButton.tsx
            src/components/productcard.module.css.
    
    * Created register action, api route to upload user image, frontend sign up page. Recovered productsModel.ts.
    
    * Improved the Login page bringing the loginc sign in the user in the 'use client' page.
    
    * Changed redirect after login to the products page and added a paragraph explaining the products on the top of the products page.
    
    * deleted a duplicated code in the sigup page
    
    * From Account branch to Test Branch (#12)
    
    * Created the api product  route, product page and changed the ProductT id to '_id'.
    
    * Added routes api/ products/[id], api/shopping-list/
            src/app/lib/actions.ts, src/app/lib/actions.ts
            src/app/products/[productId]/
            src/components/Grid.tsx
            src/components/ModalAlert.tsx
            src/components/ProductCard.tsx
            src/components/addToShoppingListButton.tsx
            src/components/productcard.module.css.
    
    * Created register action, api route to upload user image, frontend sign up page. Recovered productsModel.ts.
    
    * Improved the Login page bringing the loginc sign in the user in the 'use client' page.
    
    * Changed redirect after login to the products page and added a paragraph explaining the products on the top of the products page.
    
    * deleted a duplicated code in the sigup page
    
    * Created Account Dashboard layout. Updated the profile page with user personal data and contacts. Added api route and frontend function to update user's name. The same was done to updtae the user's address.
    
    * Created the products list page for the seller. SellerAddProduct component. Api route for adding image to the product.
    
    * Finished the 'Add product' feature with the corresponded api/products/create-product in the backend and the submitProduct function in the frontend.
    
    * Created the Seller's Store. New folders 'store' created in the api folder and the app route. Some commented code and spaces deleted in the single products pages.
    
    * added the AddToShoppingListButton with Action/Server Function to the ProductCard in the Products Page and Single Products Page.
    
    * Removed old addtoList function from the ProductCardStore
    
    * added angle bracket to link in single product page
    
    * Improved the logic of the profile page if the user is a seller, a buyer or is unauthenticated or loading while awaiting a resolution.
    
    * Fixed the missing icon in the store's map. The icons were added to the public folder and a special client component was created for the map(MapClient) and inserted in the store's page

---
+++ src/components/MapClient.tsx
@@ -0,0 +1,38 @@
+"use client";
+
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import "leaflet/dist/leaflet.css";
+import L from "leaflet";
+
+// Fix missing marker icons
+delete (L.Icon.Default.prototype as any)._getIconUrl;
+L.Icon.Default.mergeOptions({
+  iconRetinaUrl: "/leaflet/marker-icon-2x.png",
+  iconUrl: "/leaflet/marker-icon.png",
+  shadowUrl: "/leaflet/marker-shadow.png",
+});
+
+interface MapClientProps {
+  lat: number;
+  lng: number;
+  label: string;
+}
+
+export default function MapClient({ lat, lng, label }: MapClientProps) {
+  return (
+    <MapContainer
+      center={[lat, lng]}
+      zoom={15}
+      scrollWheelZoom={false}
+      style={{ height: "200px", width: "100%" }}
+    >
+      <TileLayer
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
+      <Marker position={[lat, lng]}>
+        <Popup>{label}</Popup>
+      </Marker>
+    </MapContainer>
+  );
+}
