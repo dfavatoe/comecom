96008e62604ffd97190de38b5e1a41f9532798ec
fridaSea <maria.kluge.codes@gmail.com>
2025-04-22 12:41:20 +0200

    Set up product carousel on home page

--- next.config.ts
+++ next.config.ts
@@ -1,16 +1,17 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
   images: {
     remotePatterns: [
       {
         protocol: "https",
         hostname: "cdn.dummyjson.com",
+        pathname: "**",
       },
       {
-        protocol: "http",
-        hostname: "cdn.dummyjson.com",
+        protocol: "https",
+        hostname: "product.hstatic.net",
+        pathname: "**",
       },
     ],
   },
