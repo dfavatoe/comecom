8dfad926a1718ea6b9651ad3498949898e830653
fridaSea <maria.kluge.codes@gmail.com>
2025-04-27 13:26:52 +0200

    Add handling when not logged in

--- src/app/account/dashboard/chats/page.tsx
+++ src/app/account/dashboard/chats/page.tsx
@@ -1,6 +1,37 @@
+"use client";
 import SellerChatDashboard from "@/components/SellerChatDashboard";
+import { useSession } from "next-auth/react";
 import "@/app/globals.css";
+import { useEffect, useState } from "react";
+import { Container } from "react-bootstrap";
+import Link from "next/link";
 
 export default function SellerChatPage() {
+  const { data: session, status } = useSession();
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    if (status !== "loading") {
+      setLoading(false); // Set loading to false once session status is resolved
+    }
+  }, [status]);
+
+  if (loading) return <p>Loading...</p>;
+  if (status === "unauthenticated")
+    return (
+      <Container className="d-block">
+        <h4>You must be signed in to view your account.</h4>
+        <div className="d-block mb-4">
+          <Link className="d-inline" href={"/signup"}>
+            Sign up{" "}
+          </Link>
+          <span>or </span>
+          <Link className="d-inline" href={"/login"}>
+            Log in{" "}
+          </Link>
+        </div>
+      </Container>
+    );
+
   return <SellerChatDashboard />;
 }
