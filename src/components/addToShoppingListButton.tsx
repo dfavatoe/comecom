e4ac276a0c35e49846d700171f1cee73cd742ca5
dfavatoe <d.favato@outlook.com>
2025-09-07 19:06:38 +0200

    Reactivated the addToList action in the button after discovering authentication problem.

--- src/components/addToShoppingListButton.tsx
+++ src/components/addToShoppingListButton.tsx
@@ -2,7 +2,7 @@
 
 import { useState } from "react";
 import { useSession } from "next-auth/react";
-// import { addProductToList } from "@/app/lib/actions";
+import { addProductToList } from "@/app/lib/actions";
 import { useToast } from "@/hooks/useToast";
 
 type AddToShoppingListButtonProps = {
@@ -37,58 +37,58 @@ export default function AddToShoppingListButton({
       }),
     };
 
-    try {
-      const response = await fetch(
-        `api/products-list/add-product-to-list`,
-        requestOptions
-      );
-
-      const data = await response.json();
-      console.log("API response :>> ", response.status, data);
-
-      if (response.status === 400) {
-        showToast(
-          data.message ||
-            data.error ||
-            "You already have this product in the shopping list.",
-          "warning"
-        );
-        return;
-      }
-
-      if (response.status === 404) {
-        showToast(data.error || "User not found.", "danger");
-        return;
-      }
-
-      if (!response.ok) {
-        showToast(data.error || "Something went wrong.", "danger");
-        return;
-      }
-
-      showToast(data.message || "Product successfully added!", "success");
-    } catch (error) {
-      console.log("Error adding product to list :>> ", error);
-      throw error;
-    } finally {
-      setLoading(false);
-    }
     // try {
-    //   // await addProductToList(productId);
-    //   await fetch('/api/products-list/add-product-to-list')
-    //   showToast("Product added to your shopping list!", "success");
-    // } catch (err: unknown) {
-    //   console.error("Error adding product: ", err);
+    //   const response = await fetch(
+    //     `api/products-list/add-product-to-list`,
+    //     requestOptions
+    //   );
 
-    //   let message = "Failed to add product to the shopping list.";
-    //   if (err instanceof Error) {
-    //     message = err.message;
+    //   const data = await response.json();
+    //   console.log("API response :>> ", response.status, data);
+
+    //   if (response.status === 400) {
+    //     showToast(
+    //       data.message ||
+    //         data.error ||
+    //         "You already have this product in the shopping list.",
+    //       "warning"
+    //     );
+    //     return;
     //   }
 
-    //   showToast(message, "danger");
+    //   if (response.status === 404) {
+    //     showToast(data.error || "User not found.", "danger");
+    //     return;
+    //   }
+
+    //   if (!response.ok) {
+    //     showToast(data.error || "Something went wrong.", "danger");
+    //     return;
+    //   }
+
+    //   showToast(data.message || "Product successfully added!", "success");
+    // } catch (error) {
+    //   console.log("Error adding product to list :>> ", error);
+    //   throw error;
     // } finally {
     //   setLoading(false);
     // }
+    try {
+      await addProductToList(productId);
+      // await fetch("/api/products-list/add-product-to-list");
+      showToast("Product added to your shopping list!", "success");
+    } catch (err: unknown) {
+      console.error("Error adding product: ", err);
+
+      let message = "Failed to add product to the shopping list.";
+      if (err instanceof Error) {
+        message = err.message;
+      }
+
+      showToast(message, "danger");
+    } finally {
+      setLoading(false);
+    }
   };
 
   return (
